b6fb928e5df49bb8f9521dfd6d45a45f6241519e
Vince Lundgren <vincent.lundgren@gmail.com>
2016-02-15 08:19:56 -0600

    adding reflections

---
+++ control_flow.rb
@@ -0,0 +1,63 @@
+if true
+  do something
+  end
+
+  while true
+    do something
+    increment
+    end
+
+    unless false
+      do something
+      end
+
+      until this is true
+        do this
+        end
+
+
+ true and true = true
+      else all false
+        true and nil ==  true
+       nil and true == nil
+      nil and nil == nil
+
+
+   false or false == false
+  all others are true
+ false or false == nil
+nil or false == nil
+false or nil == false
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
